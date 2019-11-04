0bec814ee8d33b359e3a79f40e20546cd1756d9a
yeyb <10219@akeparking.cn>
2019-11-04 11:38:14 +0800

    增加执行邮件和短信通知

--- pomp_read_html.py
+++ pomp_read_html.py
@@ -8,34 +8,75 @@ import re
 class MySpiderHtml():
     def __init__(self):
         self.results = []
+        self.filePath = os.path.split(os.path.realpath(__file__))[0]
 
-    def getAllData(self, file):
-        html_file = open('{}\\overview.html'.format(file), 'r', encoding="utf-8")
+    def getAllData(self):
+        html_file = open('{}\\test-output\\TEST-TestSuite.xml'.format(self.filePath), 'r', encoding="utf-8")
         html_content = html_file.read()
-        s1 = re.findall('id="meta"([\s\S]*)id="systemInfo"', html_content)
-        now = re.findall('at ([\s\S]*)\n', s1[0])
-        passed = re.findall('<td class="passed number">(.*)</td>', html_content)
-        skipped = re.findall('<td class="zero number">(.*)</td>', html_content)
-        failed = re.findall('<td class="failed number">(.*)</td>', html_content)
-        self.results.append("执行时间：{}".format(now[0]))
-        self.results.append("通过：{}".format(passed[0]))
-        self.results.append("忽略：{}".format(skipped[0]))
-        if len(failed) != 0:
-            self.results.append("失败：{}".format(failed[0]))
-        print(self.results)
+        tests = re.findall('tests="(.*)" errors', html_content)[0]
+        errors = re.findall('errors="(.*)" skipped', html_content)[0]
+        skipped = re.findall('skipped="(.*)" failures', html_content)[0]
+        failures = re.findall('failures="(.*)">', html_content)[0]
+        passed = int(tests[0]) - int(errors) - int(skipped) - int(failures)
+        self.results.append("通过：{}".format(passed))
+        self.results.append("错误：{}".format(errors))
+        self.results.append("忽略：{}".format(skipped))
+        self.results.append("失败：{}".format(failures))
+        return self.results
 
+    def writeToFile(self,file):
+        results = self.getAllData()
         if not os.path.exists('{}\\index.txt'.format(file)):
             with open('{}\\index.txt'.format(file), 'w'):
                 pass
 
         with open('{}\\index.txt'.format(file), 'w+', encoding="UTF-8") as fileObject:
-            for r in self.results:
+            for r in results:
                 fileObject.write(r + '\n')
                 fileObject.write("====================================")
+import time
+import yagmail
+import os
+"""
+author by:yyb
+descripe:发送邮件
+"""
+class Mail():
+    def __init__(self):
+        self.yag = yagmail.SMTP(user="10219@akeparking.cn", password="Ake01997", host="hwsmtp.exmail.qq.com")
+        self.tm = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
+        self.subject = "uiDaily自动化报告_"+self.tm
 
+        self.htmlBody = ",".join(MySpiderHtml().getAllData())
+        print(self.htmlBody)
+
+        self.contents = [self.htmlBody, os.path.join(os.path.dirname(__file__), 'test-output\\html\\suite1_test1_results.html')]
+
+    def send(self,receiver):
+        self.yag.send(to=receiver,subject = self.subject,contents = self.contents)
+
+
+from twilio.rest import Client
+"""
+author by:yyb
+descripe:发送手机短信
+"""
+class SMS():
+    def __init__(self):
+        self.tm = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
+
+    def sendPhoneMessage(self,toPhone):
+        sid = 'ACedd13f6bde6bdc9fb3797888dce29572'
+        token = 'cda1b783bbb549e46126a2572cd5a01f'
+        client = Client(sid, token)
+        report = ",".join(MySpiderHtml().getAllData())
+        body_message = "uiDaily自动化测试" + self.tm + " 测试结果为：【" + report + "】"
+        client.messages.create(body=body_message, from_='+12055284403', to=toPhone)
 
 if __name__ == '__main__':
-    # file = "F:\\AKETEST\\UIAUTOTEST_POMP\\test-output\\html"  # 本机
-    file = "E:\workspace\\uiDaily_test\\test-output\\html"  # 展示电脑
+    # file = "F:\\AKETEST\\UIAUTOTEST_POMP\\test-output"  # 本机
+    file = "E:\workspace\\uiDaily_test\\test-output"  # 展示电脑
     my_html = MySpiderHtml()
-    my_html.getAllData(file)
+    my_html.writeToFile(file)
+    Mail().send("10219@akeparking.cn")
+    SMS().sendPhoneMessage("+8613531412589")
